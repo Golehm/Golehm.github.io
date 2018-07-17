f7da85d4b71b8b4ea4c87bfdcfb44b4d602731ac
Golehm <grzegorz.gorski@yahoo.pl>
2018-07-17 19:50:24 +0200

    htpp bez s

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://sheltered-brushlands-80333.herokuapp.com/v1/task';//http://localhost:8080/v1/task
-  const trelloApiRoot = 'https://sheltered-brushlands-80333.herokuapp.com/v1/trello';//'http://localhost:8080/v1/trello/';
+  const apiRoot = 'http://sheltered-brushlands-80333.herokuapp.com/v1/task';//http://localhost:8080/v1/task
+  const trelloApiRoot = 'http://sheltered-brushlands-80333.herokuapp.com/v1/trello';//'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
